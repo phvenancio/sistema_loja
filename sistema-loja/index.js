7b967daab3a6bfb170b7fea62b83386609bd7281
Pedro Henrique Venâncio <160691306+phvenancio@users.noreply.github.com>
2024-10-09 22:56:08 -0300

    @phvenancio

---
+++ sistema-loja/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+const app = express();
+
+import connection from "./config/sequelize-config.js"
+import ClientesController from "./controllers/ClientesController.js";
+import ProdutosController from "./controllers/ProdutosController.js";
+import PedidosController from "./controllers/PedidosController.js";
+
+app.use(express.urlencoded({extended: false}));
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+connection.authenticate().then(() => {
+    console.log("Conexão com o banco Feita com Sucesso!");
+}).catch((error) => {
+    console.log(error);
+})
+
+connection.query("CREATE DATABASE IF NOT EXISTS lojaDw").then(() => {
+    console.log("Banco de Dados Criado com Sucesso!");
+}).catch((error) => {
+    console.log(error);
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.use("/", ClientesController);
+app.use("/", ProdutosController);
+app.use("/", PedidosController);
+
+app.get("/pedidos", (req,res) => {
+    const listaPedidos = [{numeroPedido: "1", valor: "20.00"}, {numeroPedido: "2", valor: "60.00"}, {numeroPedido: "3", valor: "72.00"}, {numeroPedido: "4", valor: "10.00"}];
+    res.render("pedidos", {listaPedidos: listaPedidos});
+});
+
+const port = 8080;
+app.listen(port, (error) => {
+    if(error){
+        console.log(`Ocorreu um erro: ${error}`);
+    } else {
+        console.log(`Servidor Iniciado com Sucesso em: http://localhost:${port}`);
+    }
+});
---
+++ sistema-loja/models/Produtos.js
@@ -0,0 +1,18 @@
+import Sequelize from "sequelize";
+import connection from "../config/sequelize-config.js";
+
+const Produto = connection.define("produtos", {
+    nomeProduto: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },preco: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },categoria: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+Produto.sync({force: false});
+export default Produto;
